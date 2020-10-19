bfedefccea72f5af662a8ff5c64864918d510b26
huangyingxiang <1486242934@qq.com>
2020-10-19 17:27:48 +0800

    员工模块2020.10.19

--- springboot-staffclient/src/main/java/com/etc/SpringbootStaffclientApplication.java
+++ springboot-staffclient/src/main/java/com/etc/SpringbootStaffclientApplication.java
@@ -2,8 +2,10 @@ package com.etc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootStaffclientApplication {
 
     public static void main(String[] args) {

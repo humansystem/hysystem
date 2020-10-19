9255619d09a4057de443ee815a553d24f0679a4d
huangyingxiang <1486242934@qq.com>
2020-10-19 16:48:16 +0800

    员工模块2020.10.19

---
+++ springboot-staffclient/src/main/java/com/etc/SpringbootStaffclientApplication.java
@@ -0,0 +1,13 @@
+package com.etc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStaffclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStaffclientApplication.class, args);
+    }
+
+}
